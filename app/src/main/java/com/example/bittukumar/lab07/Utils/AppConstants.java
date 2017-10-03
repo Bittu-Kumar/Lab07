f635e3efe59fedee58fcfdf871e0060fdc0b2410
Bittu Kumar <bittu.dakshana15@gmail.com>
2017-10-03 16:35:10 +0530

    temp

---
+++ app/src/main/java/com/example/bittukumar/lab07/Utils/AppConstants.java
@@ -0,0 +1,30 @@
+package com.example.epuser.pickcontacts.common;
+
+/**
+ * Created by Viral on 25-05-2017.
+ */
+
+public class AppConstants {
+
+    public static final String KEY_RESP_CODE = "RESP_CODE";
+    public static final String KEY_RESP_MSG ="RESP_MSG" ;
+    public static final String KEY_RESP = "RESPONSE";
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Response codes
+    ///////////////////////////////////////////////////////////////////////////
+    public static final int SUCCESS_DATA =100 ;
+    public static final String EP_SMS_HOST = "IM-ESYPAY";
+
+
+
+    public static int SUCCESS_TRANSACTION=300;
+
+    public static final String IS_LOGGED_IN = "IS_LOGGED_IN";
+    public static final String PIN = "PIN";
+    public static final String MOBILE_NUMBER = "MOBILE_NUMBER";
+    public static final String TEMP_MOBILE_NUMBER = "TEMP_MOBILE_NUMBER";
+    public static final String PATIENT_ID ="PATIENT_ID";
+    public static final String CURRENT_PATIENT_NAME = "CURRENT_PATIENT_NAME";
+    public static final String FRAGMENT_ID = "FRAGMENT_ID";
+}
